b727aa5271e668c0f8c946be0de4bc2520e8d921
yawarm <myawar@citymd.net>
2021-09-23 16:49:28 +0500

    Setup for Heroku

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -8,6 +8,7 @@ const publicDirectoryFolder = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
@@ -84,8 +85,8 @@ app.get('*', (req, res) => {
     'errorMessage': 'Page not found.'
   })
 })
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
 
-  console.log('Server is up and running at port 3000')
+  console.log('Server is up and running at port ', port)
 })
 
